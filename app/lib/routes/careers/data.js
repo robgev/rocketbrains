9d1ce8b0cf45e11f48707bfa6e7ef506c0a0191c
Robert Gevorgyan <robert1999.g@gmail.com>
2017-10-12 11:42:22 +0400

    Add detailed ced careers page

--- app/lib/routes/careers/data.js
+++ app/lib/routes/careers/data.js
@@ -3,17 +3,124 @@
 const data = {
   android: {
     title: 'Android developer',
+    overview: `
+      Rocket Brains is search of an Android developer to help build dazzling digital work.
+      As a .Android developer, you help to develop some of the finest backends in Internet history. You work as part of different
+      project teams to build a wide variety of environments for an even wider variety of projects. What’s more, you’ll get to work
+      with extremely talented coworkers at the most fun production company around.
+    `,
+    aboutRole: `
+      As Android developer, you’re a major cog in the development of the award-winning sites we produce for the world’s
+      leading creative agencies. We rely on you to develop the backend environments that run like Usain but without the slow
+      start.
+    `,
+    aboutYou: `
+      You are a practiced web developer with at least 5 years of .NET experience.
+      You have hands-on experience in building complex web applications and a solid understanding of how your work
+      integrates with other parts of a build.
+      You have expert-level knowledge of C# and SQL and are experienced working with the ASP.NET MVC framework.
+      Experience in Sitecore is a plus.
+      You are confident in developing for a wide variety of high-load, high-performance environments.
+      Your code is lean, clean and secure and of a quality that will inspire other .NET Developers for many generations to come.
+      Functional knowledge of HTML, CSS and JS comes natural to most .NET Developers and is considered a plus.
+    `,
   },
   copyright: {
     title: ' Copyrighter internshipt',
+    overview: `
+      Rocket Brains is search of an Android developer to help build dazzling digital work.
+      As a .Android developer, you help to develop some of the finest backends in Internet history. You work as part of different
+      project teams to build a wide variety of environments for an even wider variety of projects. What’s more, you’ll get to work
+      with extremely talented coworkers at the most fun production company around.
+    `,
+    aboutRole: `
+      As Android developer, you’re a major cog in the development of the award-winning sites we produce for the world’s
+      leading creative agencies. We rely on you to develop the backend environments that run like Usain but without the slow
+      start.
+    `,
+    aboutYou: `
+      You are a practiced web developer with at least 5 years of .NET experience.
+      You have hands-on experience in building complex web applications and a solid understanding of how your work
+      integrates with other parts of a build.
+      You have expert-level knowledge of C# and SQL and are experienced working with the ASP.NET MVC framework.
+      Experience in Sitecore is a plus.
+      You are confident in developing for a wide variety of high-load, high-performance environments.
+      Your code is lean, clean and secure and of a quality that will inspire other .NET Developers for many generations to come.
+      Functional knowledge of HTML, CSS and JS comes natural to most .NET Developers and is considered a plus.
+    `,
   },
   designer: {
     title: 'Digital designer',
+    overview: `
+      Rocket Brains is search of an Android developer to help build dazzling digital work.
+      As a .Android developer, you help to develop some of the finest backends in Internet history. You work as part of different
+      project teams to build a wide variety of environments for an even wider variety of projects. What’s more, you’ll get to work
+      with extremely talented coworkers at the most fun production company around.
+    `,
+    aboutRole: `
+      As Android developer, you’re a major cog in the development of the award-winning sites we produce for the world’s
+      leading creative agencies. We rely on you to develop the backend environments that run like Usain but without the slow
+      start.
+    `,
+    aboutYou: `
+      You are a practiced web developer with at least 5 years of .NET experience.
+      You have hands-on experience in building complex web applications and a solid understanding of how your work
+      integrates with other parts of a build.
+      You have expert-level knowledge of C# and SQL and are experienced working with the ASP.NET MVC framework.
+      Experience in Sitecore is a plus.
+      You are confident in developing for a wide variety of high-load, high-performance environments.
+      Your code is lean, clean and secure and of a quality that will inspire other .NET Developers for many generations to come.
+      Functional knowledge of HTML, CSS and JS comes natural to most .NET Developers and is considered a plus.
+    `,
   },
   frontend: {
     title: 'Frontend developer',
+    overview: `
+      Rocket Brains is search of an Android developer to help build dazzling digital work.
+      As a .Android developer, you help to develop some of the finest backends in Internet history. You work as part of different
+      project teams to build a wide variety of environments for an even wider variety of projects. What’s more, you’ll get to work
+      with extremely talented coworkers at the most fun production company around.
+    `,
+    aboutRole: `
+      As Android developer, you’re a major cog in the development of the award-winning sites we produce for the world’s
+      leading creative agencies. We rely on you to develop the backend environments that run like Usain but without the slow
+      start.
+    `,
+    aboutYou: `
+      You are a practiced web developer with at least 5 years of .NET experience.
+      You have hands-on experience in building complex web applications and a solid understanding of how your work
+      integrates with other parts of a build.
+      You have expert-level knowledge of C# and SQL and are experienced working with the ASP.NET MVC framework.
+      Experience in Sitecore is a plus.
+      You are confident in developing for a wide variety of high-load, high-performance environments.
+      Your code is lean, clean and secure and of a quality that will inspire other .NET Developers for many generations to come.
+      Functional knowledge of HTML, CSS and JS comes natural to most .NET Developers and is considered a plus.
+    `,
   },
   illustrator: {
     title: 'Illustrator',
+    overview: `
+      Rocket Brains is search of an Android developer to help build dazzling digital work.
+      As a .Android developer, you help to develop some of the finest backends in Internet history. You work as part of different
+      project teams to build a wide variety of environments for an even wider variety of projects. What’s more, you’ll get to work
+      with extremely talented coworkers at the most fun production company around.
+    `,
+    aboutRole: `
+      As Android developer, you’re a major cog in the development of the award-winning sites we produce for the world’s
+      leading creative agencies. We rely on you to develop the backend environments that run like Usain but without the slow
+      start.
+    `,
+    aboutYou: `
+      You are a practiced web developer with at least 5 years of .NET experience.
+      You have hands-on experience in building complex web applications and a solid understanding of how your work
+      integrates with other parts of a build.
+      You have expert-level knowledge of C# and SQL and are experienced working with the ASP.NET MVC framework.
+      Experience in Sitecore is a plus.
+      You are confident in developing for a wide variety of high-load, high-performance environments.
+      Your code is lean, clean and secure and of a quality that will inspire other .NET Developers for many generations to come.
+      Functional knowledge of HTML, CSS and JS comes natural to most .NET Developers and is considered a plus.
+    `,
   },
-}
+};
+
+export default data;
--- app/lib/routes/careers/detailed.js
+++ app/lib/routes/careers/detailed.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Layout from '../../components/layout';
+import Article from '../../components/article';
+import data from './data';
 
-const DetailedJobPosting = ({ match }) => (
-  <Layout className="careers">
-    <p>Detailed page of {match.params.jobID}</p>
-  </Layout>
-);
+const DetailedJobPosting = ({ match }) => {
+  const jobInfo = data[match.params.jobID];
+  const { title, overview, aboutRole, aboutYou } = jobInfo;
+  return (
+    <Layout className="careers">
+      <Article
+        title={title}
+        imageSource='/images/careers.png'
+        style={{paddingBottom: '300px'}}
+      >
+        <p>{overview}</p>
+        <p><strong>About Role</strong></p>
+        <p>{aboutRole}</p>
+        <p><strong>About You</strong></p>
+        <p>{aboutYou}</p>
+        <p></p>
+      </Article>
+    </Layout>
+  );
+}
 
 export default DetailedJobPosting
