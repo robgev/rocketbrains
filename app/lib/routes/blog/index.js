8804fe884c76782331ae67236d9ba1c3a6d6705c
Robert Gevorgyan <robert1999.g@gmail.com>
2018-01-03 18:41:11 +0400

    Adjust heights

--- app/lib/routes/blog/index.js
+++ app/lib/routes/blog/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from 'components/layout';
 import Tiles from 'components/tiles';
 import data from './data';
-import { fillArrayWithEmptyTiles } from 'utils';
 
 import 'scss/blogPost.scss';
 
